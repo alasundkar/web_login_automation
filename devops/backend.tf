c491e7369a72f1235fd9c3cfc76a019d9aa6ba9d
rupesh <rupeshalasundkar@gmail.com>
2021-06-11 08:09:36 -0700

    custom changes

--- devops/backend.tf
+++ devops/backend.tf
@@ -3,7 +3,7 @@
 #  backend "s3" {
 #    bucket = "jms-terraform-backend"
 #    key    = "jmsth_jenkins.tfstate"
-#    region = "ap-south-1"
+#    region = "eu-west-1"
 #    encrypt = true
 #    dynamodb_table = "terraform-state-lock-dynamo"
 #    }
